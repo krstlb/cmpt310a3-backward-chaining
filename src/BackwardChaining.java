0ce1c3bd224872a1047351e4b4aab47a797da237
Krystle Bulalakaw <krystle.bulalakaw@gmail.com>
2016-11-18 20:24:34 -0800

    Added BC logic

--- src/BackwardChaining.java
+++ src/BackwardChaining.java
@@ -12,15 +12,31 @@ public class BackwardChaining {
     private static ArrayList<String> entailed;
 
     public static void main(String[] args) {
+        String output = "";
 
         init(args[0]);
 
         System.out.println();
-        System.out.println("query: " + query);
+        System.out.println("First query: " + query);
         System.out.println();
 
-        //backwardChaining();
+        System.out.println("Starting backward chaining...");
 
+        if (backwardChaining()) {
+            output +=  query + " is TRUE\n";
+            output +=  "order of entailment:\n";
+            // append the entailed symbols in reverse to output
+            for (int i = entailed.size() - 1; i >= 0; i--) {
+                output += entailed.get(i);
+                if (i != 0) {
+                    output += ", ";
+                }
+            }
+        } else {
+            output += query + " is FALSE\n";
+        }
+
+        System.out.println(output);
     }
 
     /**
@@ -50,9 +66,9 @@ public class BackwardChaining {
             // go to next line (start of rules)
             scan.nextLine();
 
-            System.out.println("Reading rules...");
+            System.out.println("Reading input...");
             System.out.println();
-
+            System.out.println("RULES:");
             // get rules
             int numLines = 1;
             while (scan.hasNextLine()) {
@@ -85,10 +101,75 @@ public class BackwardChaining {
         }
     }
 
+    /**
+     * Works backwards from query q. Tries to prove q by checking if q is known or, proves recursively by BC
+     * all the premises of some rule concluding q.
+     * It avoid loops by checking if new suboal is already in the set of goals. It avoids repeating work by
+     * checking if the new subgoal has already been proven true, or has already failed.
+     * @return
+     */
     private static boolean backwardChaining() {
         while (!goals.isEmpty()) {
+            System.out.println();
+            System.out.println("Goals to evaluate: " + goals);
+            // Get the current predicate that is our goal
+            String q = goals.remove(goals.size() - 1);
 
+            System.out.println("Evaluating " + q + "...");
+            // Add the entailment to keep track of processed predicates
+            entailed.add(q);
+
+            // If this predicate is not already a proven fact, we process it
+            if (!facts.contains(q)) {
+                ArrayList<String> predicatesToBeProcessed = new ArrayList<String>();
+                // For each clause that contains the predicate as its conclusion
+                // add the symbols to the list of predicates to be processed
+                for (int i = 0; i < clauses.size(); i++) {
+                    String c = clauses.get(i);
+                    if (checkConclusionContains(c, q)) {
+                        String[] predicates = c.split(" ");
+                        System.out.println("Found " + q + " as conclusion in (" + c + ")");
+                        for (int j = 1; j < predicates.length; j++) {
+                            if (!predicatesToBeProcessed.contains(predicates[j])) {
+                                predicatesToBeProcessed.add(predicates[j]);
+                            }
+                        }
+                    }
+                }
+
+                // Since no predicates were found to process, and the query is not a fact,
+                // we cannot prove this query
+                if (predicatesToBeProcessed.size() == 0) {
+                    return false;
+                }
+                // There are predicates to process. If they are not already entailed,
+                // add them to the list of goals
+                else {
+                    for (int i = 0; i < predicatesToBeProcessed.size(); i++) {
+                        if (!entailed.contains(predicatesToBeProcessed.get(i)) &&
+                                !goals.contains(predicatesToBeProcessed.get(i))) {
+                            goals.add(predicatesToBeProcessed.get(i));
+                        }
+                    }
+                }
+            } else {
+                System.out.println(q + " evaluated to be TRUE");
+            }
+            System.out.println("Visited: " + entailed);
         }
         return true;
     }
+
+    /**
+     * Checks if query appears in the conclusion of a given clause.
+     * @param c input clause
+     * @param q query to check if it is contained in the given conclusion
+     * @return true if conclusion contains the query, else false
+     */
+    private static boolean checkConclusionContains(String c, String q) {
+        String conclusion = c.split(" ")[0];
+        return conclusion.equals(q);
+    }
+
+
 }
